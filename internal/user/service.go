c6719d3ead4f0b4b7d0d22356bda983cd6305400
Kranti <darkphoton20@gmail.com>
2024-11-10 21:19:17 +0800

    fix: removing password

--- internal/user/service.go
+++ internal/user/service.go
@@ -56,6 +56,8 @@ func (s *UserService) LoginUserService(loginReq UserLoginRequest) (*UserLoginRes
 	refreshExpiryTime := time.Hour * 24 * 7
 	refreshToken, err := auth.GenerateJWT(*user, auth.Refresh, refreshExpiryTime)
 
+	user.Password = ""
+
 	res := &UserLoginResponse{
 		AccessToken:      accessToken,
 		AccessExpiresIn:  int(accessExpiryTime),
