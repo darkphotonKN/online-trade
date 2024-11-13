88cccf2404c3af4aeb1d602b9a35b2d77fbc1a58
Kranti <darkphoton20@gmail.com>
2024-11-13 23:05:25 +0800

    feat: Updating item migration, types, and models to reflect item categorization.

--- internal/item/model.go
+++ internal/item/model.go
@@ -1,6 +1,8 @@
 package item
 
 type UpdateItemReq struct {
+	Category      string  `db:"category" json:"category"`
+	Type          string  `db:"type" json:"type"`
 	Name          string  `db:"name" json:"name"`
 	Description   string  `db:"description" json:"description"`
 	PricePerUnit  float64 `db:"price_per_unit" json:"pricePerUnit"`
--- migrations/20241109120049_create_items.sql
+++ migrations/20241109120049_create_items.sql
@@ -4,11 +4,13 @@
 CREATE TABLE IF NOT EXISTS items (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES users(id) ON DELETE CASCADE, -- Links item to the user listing it
-    product_id UUID NOT NULL,  -- Unique identifier for each product type
-    name VARCHAR(255) NOT NULL,  -- Name of the product
-    description TEXT,  -- Description of the product
+    item_id UUID NOT NULL,  -- Unique identifier for each item type
+    name VARCHAR(255) NOT NULL,  -- Name of the item
+    description TEXT,  -- Description of the item
     price_per_unit DECIMAL(10, 2) NOT NULL,  -- Price for each unit
     stock_quantity INT NOT NULL CHECK (stock_quantity >= 0),  -- Number of items available for sale
+    category VARCHAR(255) NOT NULL,  -- Broad Category of Item
+    type VARCHAR(255) NOT NULL,  -- Specific Type of Item
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
